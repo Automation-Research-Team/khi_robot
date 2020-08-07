52621d2d34223cb8e08ae5a9b37b504b7458627f
Toshio UESHIBA <t.ueshiba@aist.go.jp>
2020-08-07 21:11:39 +0900

    Add pulse() member function

--- khi_robot_control/include/khi_robot_driver_plugin.h
+++ khi_robot_control/include/khi_robot_driver_plugin.h
@@ -17,21 +17,24 @@ namespace khi_robot_control
 class DriverPlugin
 {
   public:
-    using dio_t	= std::array<uint8_t, 64>;
+    using dio_t	= std::array<uint8_t, 64>;	// 64*8 = 512 signals
 
   public:
+		DriverPlugin()	:_cont_no(0), _driver(nullptr)		{}
+    
     void	init(int cont_no, KhiRobotDriver* driver)		;
 
   protected:
     void	clear()							;
     bool	read()							;
     void	write()						const	;
+    void	pulse(double sec=0.2)					;
     template <class T>
     T		in(int offfset)					const	;
     template <class T>
     T		out(int offfset)				const	;
     template <class T>
-    void	set_out(int offfset, T val)				;
+    void	set_out(int offfset, T val, T mask=~0)			;
     template <class T>
     void	clear_mask(int offfset)					;
 
@@ -72,6 +75,15 @@ DriverPlugin::write() const
     _driver->setDIO(_cont_no, _out.data(), _mask.data());
 }
 
+inline void
+DriverPlugin::pulse(double sec)
+{
+    write();
+    ros::Duration(sec).sleep();
+    std::fill(std::begin(_out), std::end(_out), 0);
+    write();
+}
+
 template <class T> inline T
 DriverPlugin::in(int offset) const
 {
@@ -84,14 +96,14 @@ DriverPlugin::out(int offset) const
     return *reinterpret_cast<const T*>(_out.data() + offset);
 }
     
-template <class T> void
-DriverPlugin::set_out(int offset, T val)
+template <class T> inline void
+DriverPlugin::set_out(int offset, T val, T mask)
 {
     *reinterpret_cast<T*>(_out.data()  + offset) = val;
-    *reinterpret_cast<T*>(_mask.data() + offset) = T(~0);
+    *reinterpret_cast<T*>(_mask.data() + offset) = mask;
 }
     
-template <class T> void
+template <class T> inline void
 DriverPlugin::clear_mask(int offset)
 {
     *reinterpret_cast<T*>(_mask.data() + offset) = T(0);
